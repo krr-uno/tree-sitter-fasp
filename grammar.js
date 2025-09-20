87ae84e999b9ac76f2ea43f3b1ce5829375939ff
Jorge Fandinno <jorgefandinno@gmail.com>
2025-09-20 11:03:02 -0500

    some and new choice

--- grammar.js
+++ grammar.js
@@ -10,14 +10,22 @@ module.exports = grammar(clingo, {
     ],
     
     rules: {
-        aggregate_assignment_aggregate: $ => seq($.aggregate_function, "{", optional($.body_aggregate_elements), "}"),
-        choice_assignment_aggregate: $ => seq("{", $.body_aggregate_elements, "}"),
+        simple_assignment: $ => seq($.term, ":=", $.term),       
 
-        simple_assignment: $ => seq($.term, ":=", $.term),
-        aggregate_assignment: $ => seq($.term, ":=", $.aggregate_assignment_aggregate),
-        choice_assignment: $ => seq($.term, "in", $.choice_assignment_aggregate),
+        _aggregate_assignment: $ => seq($.aggregate_function, "{", optional($.body_aggregate_elements), "}"),
+        aggregate_assignment: $ => seq($.term, ":=", $._aggregate_assignment),
 
-        _head_assignment: $ => choice($.simple_assignment, $.aggregate_assignment, $.choice_assignment),
+        _choice_some_assignment: $ => seq("{", $.body_aggregate_elements, "}"),
+        choice_some_assignment: $ => seq($.term, "#some", $._choice_some_assignment),
+
+        choice_assignment_element: $ => seq(
+            field("assignment", $.simple_assignment),
+            field("condition", optional($._condition))),
+        choice_assignment_elements: $ => seq($.choice_assignment_element, repeat(seq(";", $.choice_assignment_element))),
+        _choice_assignment: $ => seq("{", $.choice_assignment_elements, "}"),
+        choice_assignment: $ => seq(optional($.lower), $._choice_assignment, optional($.upper)),
+
+        _head_assignment: $ => choice($.simple_assignment, $.aggregate_assignment, $.choice_some_assignment, $.choice_assignment),
 
         assignment_rule: $ => seq($._head_assignment, choice(".", seq(":-", $.body))),
 
